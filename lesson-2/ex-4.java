6d67682544f2f02d6a7696d6b1bc2eca192b78f0
WlaBea <92812560+WlaBea@users.noreply.github.com>
2022-11-05 14:55:04 +0100

    create ex-4 task

---
+++ lesson-2/ex-4.java
@@ -0,0 +1,26 @@
+public class myClass{
+
+    interface Book{
+        Book convertPrice(float rate);
+    }
+    public static class NewBook implements Book{
+        private final float price;
+        NewBook(float initPrice){
+            this.price=initPrice;
+        }
+        public String printPrice() {
+            return String.format("The book costs %f", this.price);
+        }
+        @Override
+        public Book convertPrice(float rate){
+            return new NewBook(this.price * rate);
+        }
+    }
+     public static void main(String []args){
+        NewBook book = new NewBook(25);
+        //NewBook bookEuro = book.convertPrice(4);
+        /*error: incompatible types: Book cannot be converted to NewBook*/
+        System.out.println(book.printPrice());
+        //System.out.println(bookEuro.printPrice());
+     }
+}
